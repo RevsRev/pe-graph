7e4ee919df27eb09927bfb035bb169975290996e
RevsRev <eddie.revell@yahoo.co.uk>
2025-01-04 14:33:14 +0000

    Sorted out gitignore and implemented parabola example

--- rev-plot-examples/parabola/src/main/java/github/com/rev/plot/example/parabola/Main.java
+++ rev-plot-examples/parabola/src/main/java/github/com/rev/plot/example/parabola/Main.java
@@ -1,7 +1,33 @@
 package github.com.rev.plot.example.parabola;
 
+import github.com.rev.plot.example.parabola.plot.canvas.Canvas;
+import github.com.rev.plot.example.parabola.plot.graphable.Graphable;
+import github.com.rev.plot.example.parabola.plot.graphable.impl.GraphableAxes;
+import github.com.rev.plot.example.parabola.plot.graphable.impl.func.GraphableFunc;
+import github.com.rev.plot.example.parabola.plot.graphics.impl.GraphicsLinear;
+import github.com.rev.plot.example.parabola.plot.ui.GraphFrame;
+import github.com.rev.plot.example.parabola.plot.ui.GraphFrameUpdateRunnable;
+import rev.pe.math.linear.vec.Vec2;
+
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        GraphicsLinear graphicsT = new GraphicsLinear(new Vec2(1,0), new Vec2(0,1));
+        Canvas canvas = new Canvas(graphicsT);
+        canvas.setCanvasCalc(new Rectangle2D.Double(-40,-40,80,80));
+        canvas.setCanvasWindow(new Rectangle2D.Double(-20,-20,40,40));
+        GraphFrame frame = new GraphFrame(1600, 800, canvas);
+
+        Graphable axes = new GraphableAxes();
+        axes.setColor(Color.LIGHT_GRAY);
+        canvas.addGraphable(axes);
+
+        canvas.addGraphable(GraphableFunc.factory(GraphableFunc.FuncTypes.PARABOLA));
+
+        GraphFrameUpdateRunnable.startGraphUpdateThread(frame);
+
+        frame.setVisible(true);
     }
 }
